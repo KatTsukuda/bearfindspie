720b334313f0876105ac8d4ad46f9b21fecce968
kat tsukuda <bearheartsbat@gmail.com>
2017-06-14 12:19:50 -0700

    add huge arse pie

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import bear from './goldenbear.svg';
 import './App.css';
+import pie from './pie.svg';
 
 class App extends Component {
   render() {
@@ -9,9 +10,10 @@ class App extends Component {
         <div className="App-header">
           <h2>Bear Finds Pie</h2>
         </div>
-        <div><img src={bear} className="bear" alt="bear"/></div>
+        <div><img src={bear} className="bear" alt="bear" /></div>
         <div className="maze-block one"></div>
         <div className="maze-block two"></div>
+        <div><img src={pie} className="pie" alt="pie" /></div>
       </div>
     );
   }
